457a5b75251d700b1b3ef3d4d9ae24692c713808
Romain Caillat <caillat.romain@epitech.eu>
2021-07-28 16:23:34 +0200

    [ADD]: Material-Ui

--- src/index.js
+++ src/index.js
@@ -6,11 +6,19 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Typography from '@material-ui/core/Typography';
 
 const shops = [
   {
     id:1,
     name: "TEST",
+    date: "20/11/2002",
     link: "https://youtube.com/"
   },
   {
@@ -22,6 +30,36 @@ const shops = [
     id: 3,
     name:"TEST3",
     link: "https://youtube.com/"
+  },
+  {
+    id: 4,
+    name:"TEST4",
+    link: "https://youtube.com/"
+  },
+  {
+    id: 5,
+    name:"TEST5",
+    link: "https://youtube.com/"
+  },
+  {
+    id: 6,
+    name:"TEST6",
+    link: "https://youtube.com/"
+  },
+  {
+    id: 7,
+    name:"TEST7",
+    link: "https://youtube.com/"
+  },
+  {
+    id: 8,
+    name:"TEST8",
+    link: "https://youtube.com/"
+  },
+  {
+    id: 9,
+    name:"TEST9",
+    link: "https://youtube.com/"
   }
 ]
 
@@ -29,19 +67,11 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+        <div style={{ background: '#607d8b', width: 1200, height: 75, marginLeft: 250 }}>
+          <Typography style={{marginLeft: 500}} component="h2" variant="h6" color="primary" gutterBottom>
+            Liste des sites
+          </Typography>
+        </div>
         <Switch>
             {shops.map (shop => {
               return PersonalUsers(shop)
@@ -52,11 +82,6 @@ export default function App() {
           <Route path="/">
             <Home />
           </Route>
-          {/*<div>
-            {shops.map (shop => {
-              return PersonalUsers(shop)
-            })}
-          </div>*/}
         </Switch>
       </div>
     </Router>
@@ -72,24 +97,35 @@ function PersonalUsers(shop)
   )
 }
 
-function DrawHome(shop) {
+function DrawHome(shops) {
   return (
-    <div>
-      <div style={{ background: '#000000', width: 500, height: 3, marginLeft: 30 }}/>
-      <div style={{ background: '#607d8b', width: 500, height: 75, marginLeft: 30 }}>
-        <pre style={{ margin: 0, padding: 0, border: 0, width: 10, height: 0}}>{shop.name}         <a href={shop.link}>{shop.link}</a>    <Link to={`/users/${shop.id}`} activeClassName="current">user</Link></pre>
-        <div style={{ background: '#000000', width: 3, height: 75, marginLeft: 80 }}/>
-      </div>
-    </div>
+    <React.Fragment>
+      <Table size="small" padding = 'normal' stickyHeader={true} height={10} width={10} rowHeight={10}  style={{ width: 1200, marginLeft: 250, background: '#90caf9'}}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Date</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>link User</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {shops.map((shop) => (
+            <TableRow key={shop.id}>
+              <TableCell>{shop.date}</TableCell>
+              <TableCell>{shop.name}</TableCell>
+              <TableCell><Link to={`/users/${shop.id}`} activeClassName="current">user</Link></TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </React.Fragment>
   )
 }
 
 function Home() {
   return (
     <div className="MainPage">
-      {shops.map(shop => {
-            return DrawHome(shop)
-      })}
+      {DrawHome(shops)}
     </div>
   )
 }
@@ -104,6 +140,7 @@ function Users(shop) {
       <h2>NEW USER</h2>
       <h2>{shop.name}</h2>
       <h2>{shop.link}</h2>
+      <Link to="/">Home</Link>
     </div>
   )
 }
