47ba11241c962a4707a58725f7ebd225d6d9c6d3
Romain Caillat <caillat.romain@epitech.eu>
2021-07-28 16:31:30 +0200

    [DELETE]: useless defined

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
